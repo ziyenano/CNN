05a4462cd50c38e5e58704370af93a812cd9dee0
xiangcai <ziyenano@gmail.com>
2017-01-23 09:41:35 +0800

    change the code format

--- conv.c
+++ conv.c
@@ -3,195 +3,173 @@
 #include "Rinternals.h"
 #include "stdlib.h"
 
-void valid_conv(double * A, double * B,int m_A, int n_A,int m_B,int n_B, \
-	double * C)
+void valid_conv(double *A, double *B, int m_A, int n_A,int m_B, int n_B, \
+        double *C)
 {
-  int i,j,m,n;
-  for(i=0;i<m_A-m_B+1;++i)
-  {
-  	for(j=0;j<n_A-n_B+1;++j)
-  	{
-  		double s=0;
-  		for(m=0;m<m_B;++m)
-  		{
-  			for(n=0;n<n_B;++n)
-  			{
-  				s+=A[(m+i)*n_A+n+j]*B[(m_B-m-1)*n_B+n_B-n-1];
-  				//s+=1;
-  			}
-  		}
-  	C[i*(n_A-n_B+1)+j]=s;
-  	}
-  }
-}
-
-void full_conv(double * A, double * B,int m_A, int n_A,int m_B,int n_B, \
-	double * C)
-{
-  int i,j,m,n;
-  int l_mB,l_nB;  
-  for(i=0;i<m_A+m_B-1;++i)
-  {
-    l_mB=(i<m_A)?i+1:m_B-i+m_A-1;
-    l_mB=(m_B<l_mB)?m_B:l_mB;
-
-  	for(j=0;j<n_A+n_B-1;++j)
-  	{	
-  		l_nB=(j<n_A)?j+1:n_B-j+n_A-1;
-  		l_nB=(n_B<l_nB)?n_B:l_nB;
-  		double s=0;
-  		for(m=0;m<l_mB ;++m)
-  		{
-  			for(n=0;n<l_nB;++n)
-  			{
-  				 // s+=A[(m+i)*n_A+n+j]*B[];
-  				 s+=1;
-  			}
-  		}
-  	C[i*(n_A+n_B-1)+j]=s;
-  	}
-  }
-}
-
-void full_conv2(double * A, double * B, int m_A,\
- int n_A,int m_B,int n_B, double * C)  
- //mainpulate full convolution by aid of vaild mode
-{
-  int i,j;
-  int nrow=2*m_B-2+m_A;
-  int ncol=2*n_B-2+n_A;
-  // double expand_A[nrow*ncol];
-  double * Expand_A=(double *)calloc(nrow*ncol,sizeof(double));
-  int cnt=0;
-  for (i = m_B-1; i <m_A+m_B-1 ; ++i)
+    int i, j, m, n;
+    for (i = 0; i < m_A - m_B + 1; ++i)
     {
-      for (j =n_B-1; j <n_A+n_B-1; ++j)
+        for (j = 0; j < n_A - n_B + 1; ++j)
         {
-          Expand_A[i*ncol+j]=A[cnt];
-          ++cnt;
+            double s = 0;
+            for (m = 0; m < m_B; ++m)
+            {
+                for (n = 0; n < n_B; ++n)
+                {
+                    s += A[(m + i) * n_A + n + j] * B[(m_B - m - 1) * n_B + n_B - n - 1];
+                    //s += 1;
+                }
+            }
+            C[i * (n_A - n_B + 1) + j] = s;
+        }
+    }
+}
+
+void full_conv(double *A, double *B, int m_A, int n_A, int m_B, int n_B, \
+        double *C)
+{
+    int i, j, m, n;
+    int l_mB, l_nB;  
+    for (i = 0; i < m_A + m_B - 1; ++i)
+    {
+        l_mB = (i < m_A) ? i + 1 : m_B - i + m_A -1;
+        l_mB = (m_B < l_mB) ? m_B : l_mB;
+
+        for (j = 0; j < n_A + n_B - 1; ++j)
+        {	
+            l_nB = (j < n_A) ? j + 1 : n_B - j + n_A - 1;
+            l_nB = (n_B < l_nB) ? n_B : l_nB;
+            double  s = 0;
+            for (m = 0; m < l_mB; ++m)
+            {
+                for (n = 0; n < l_nB; ++n)
+                {
+                    // s += A[(m + i) * n_A + n + j] * B[];
+                    s += 1;
+                }
+            }
+            C[i * (n_A + n_B - 1) + j] = s;
+        }
+    }
+}
+
+void full_conv2(double *A, double *B, int m_A,\
+        int n_A, int m_B, int n_B, double *C)  
+//mainpulate full convolution by aid of vaild mode
+{
+    int i, j;
+    int nrow = 2 * m_B - 2 + m_A;
+    int ncol = 2 * n_B - 2 + n_A;
+    // double expand_A[nrow * ncol];
+    double *Expand_A = (double *)calloc(nrow * ncol, sizeof(double));
+    int cnt = 0;
+    for (i = m_B - 1; i < m_A + m_B - 1; ++i)
+    {
+        for (j = n_B - 1; j < n_A + n_B - 1; ++j)
+        {
+            Expand_A[i * ncol + j] = A[cnt];
+            ++cnt;
         } 
     }
-  valid_conv(Expand_A,B,nrow,ncol,m_B,n_B,C);
-  free(Expand_A);
+    valid_conv(Expand_A, B, nrow, ncol, m_B, n_B, C);
+    free(Expand_A);
 }
 
 
-void same_conv(double * A, double * B, int m_A,\
- int n_A,int m_B,int n_B, double * C)  
- //mainpulate same convolution by aid of vaild mode
+void same_conv(double *A, double *B, int m_A,\
+        int n_A, int m_B, int n_B, double *C)  
+//mainpulate same convolution by aid of vaild mode
 {
-  int i,j;
-  int nrow=m_B+m_A-1;
-  int ncol=n_B+n_A-1;
-  int left_B=(int)(n_B/2);
-  int up_B=(int)(m_B/2);
-  double * Expand_A=(double *)calloc(nrow*ncol,sizeof(double));
-  int cnt=0;
-  for (i = left_B; i <m_A+left_B; ++i)
+    int i, j;
+    int nrow = m_B + m_A - 1;
+    int ncol = n_B + n_A - 1;
+    int left_B = (int)(n_B / 2);
+    int up_B = (int)(m_B / 2);
+    double *Expand_A = (double *)calloc(nrow * ncol, sizeof(double));
+    int cnt = 0;
+    for (i = left_B; i < m_A + left_B; ++i)
     {
-      for (j =up_B; j <n_A+up_B; ++j)
+        for (j = up_B; j <n_A + up_B; ++j)
         {
-          Expand_A[i*ncol+j]=A[cnt];
-          ++cnt;
+            Expand_A[i * ncol + j] = A[cnt];
+            ++cnt;
         } 
     }
-  valid_conv(Expand_A,B,nrow,ncol,m_B,n_B,C);
-  free(Expand_A);
+    valid_conv(Expand_A, B, nrow, ncol, m_B, n_B, C);
+    free(Expand_A);
 }
 
 
-void valid_conv_ori(double * A, double * B,int *ma, int *na,int * mb,int * nb, \
-	double * C)
+void valid_conv_ori(double *A, double *B,int *ma, int *na, int *mb, int *nb, \
+        double *C)
 {
-  int i,j,m,n;
-  int m_A=*ma,m_B=*mb,n_A=*na,n_B=*nb;
-  for(i=0;i<m_A-m_B+1;++i)
-  {
-  	for(j=0;j<n_A-n_B+1;++j)
-  	{
-  		double s=0;
-  		for(m=0;m<m_B;++m)
-  		{
-  			for(n=0;n<n_B;++n)
-  			{
-  				s+=A[(m+i)*n_A+n+j]*B[(m_B-m-1)*n_B+n_B-n-1];
-  				//s+=1;
-  			}
-  		}
-  	C[i*(n_A-n_B+1)+j]=s;
-  	}
-  }
+    int i, j, m, n;
+    int m_A = *ma, m_B = *mb, n_A = *na, n_B = *nb;
+    for (i = 0; i < m_A-  m_B + 1; ++i)
+    {
+        for (j = 0; j < n_A - n_B + 1; ++j)
+        {
+            double s = 0;
+            for (m = 0; m < m_B; ++m)
+            {
+                for (n = 0; n < n_B; ++n)
+                {
+                    s += A[(m + i) * n_A + n + j] * B[(m_B - m - 1) * n_B + n_B - n - 1];
+                }
+            }
+            C[i * (n_A - n_B + 1) + j] = s;
+        }
+    }
 }
 
 
-// int main()
-// {
-// double a[]={1,2,3,4,5,6,7,8,9};
-// double b[]={1,2,3,4};
-// int ma=3,na=3;
-// int mb=2,nb=2;
-// // double c[(ma-mb+1)*(na-nb+1)];
-// // valid_conv(a,b,ma,na,mb,nb,c);
-// // int i;
-// // for(i=0;i<(ma-mb+1)*(na-nb+1);++i)
-// // 	printf("%5.2lf \n",c[i]);
-// double c[(ma+mb-1)*(na+nb-1)];
-// double expand_a[(2*mb-2+ma)*(2*nb-2+na)];
-// full_conv2(a,b,expand_a,ma,na,mb,nb,2*mb-2+ma,2*nb-2+na,c);
-// int i;
-// for(i=0;i<(ma+mb-1)*(na+nb-1);++i)
-// 	printf("%5.2lf \n",c[i]);
-// return 0;
-// }
-
-SEXP r_conv(SEXP r_A, SEXP r_B, SEXP r_m_A, SEXP r_n_A,SEXP r_m_B, SEXP r_n_B,SEXP r_mode)
+SEXP r_conv(SEXP r_A, SEXP r_B, SEXP r_m_A, SEXP r_n_A, SEXP r_m_B, SEXP r_n_B, SEXP r_mode)
 {
-  double *A=REAL(r_A);
-  double *B=REAL(r_B);
-  int m_A=INTEGER(r_m_A)[0]; 
-  int n_A=INTEGER(r_n_A)[0]; 
-  int m_B=INTEGER(r_m_B)[0];
-  int n_B=INTEGER(r_n_B)[0];
-  int mode=INTEGER(r_mode)[0];
-  SEXP conv;
-  double *p;
-  int len;
-  //valid mode 
-  if(mode==1) 
-  {
-    len=(m_A-m_B+1)*(n_A-n_B+1);
-    PROTECT(conv=allocVector(REALSXP,len));
-    p=REAL(conv);
-    valid_conv(A,B,m_A,n_A,m_B,n_B,p);
-    UNPROTECT(1);
-    return conv;
-  }
-  //full mode 
-  else if(mode==2)  
-  {
-    len=(m_A+m_B-1)*(n_A+n_B-1);
-    PROTECT(conv=allocVector(REALSXP,len));
-    p=REAL(conv);
-    full_conv2(A,B,m_A,n_A,m_B,n_B,p);
-    UNPROTECT(1);
-    return conv;
-  }
-  //same mode
-  else if(mode==3)  
-  {
-    len=m_A*n_A;
-    PROTECT(conv=allocVector(REALSXP,len));
-    p=REAL(conv);
-    same_conv(A,B,m_A,n_A,m_B,n_B,p);
-    UNPROTECT(1);
-    return conv;
-  }
+    double *A = REAL(r_A);
+    double *B = REAL(r_B);
+    int m_A = INTEGER(r_m_A)[0]; 
+    int n_A = INTEGER(r_n_A)[0]; 
+    int m_B = INTEGER(r_m_B)[0];
+    int n_B = INTEGER(r_n_B)[0];
+    int mode = INTEGER(r_mode)[0];
+    SEXP conv;
+    double *p;
+    int len;
+    //valid mode 
+    if (mode == 1) 
+    {
+        len = (m_A - m_B + 1) * (n_A - n_B + 1);
+        PROTECT(conv = allocVector(REALSXP, len));
+        p = REAL(conv);
+        valid_conv(A, B, m_A, n_A, m_B, n_B, p);
+        UNPROTECT(1);
+        return conv;
+    }
+    //full mode 
+    else if (mode == 2)  
+    {
+        len = (m_A + m_B - 1) * (n_A + n_B - 1);
+        PROTECT(conv = allocVector(REALSXP, len));
+        p = REAL(conv);
+        full_conv2(A, B, m_A, n_A, m_B, n_B, p);
+        UNPROTECT(1);
+        return conv;
+    }
+    //same mode
+    else if (mode == 3)  
+    {
+        len = m_A * n_A;
+        PROTECT(conv = allocVector(REALSXP, len));
+        p = REAL(conv);
+        same_conv(A, B, m_A, n_A, m_B, n_B, p);
+        UNPROTECT(1);
+        return conv;
+    }
 
-  PROTECT(conv=allocVector(INTSXP,1));
-  INTEGER(conv)[0]=0;
-  UNPROTECT(1);
-  return conv;
+    PROTECT(conv = allocVector(INTSXP, 1));
+    INTEGER(conv)[0] = 0;
+    UNPROTECT(1);
+    return conv;
 }
-
 
 
--- pooling.c
+++ pooling.c
@@ -1,112 +1,109 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
-void max(double * A, int len, double * max_value)
+void max(double *A, int len, double *max_value)
 {
-	int i;
-	*max_value=A[0];
-	for (i = 1; i < len; ++i)
-	{
-		if(A[i]>*max_value) 
-			*max_value=A[i];
-	}
+    int i;
+    *max_value = A[0];
+    for (i = 1; i < len; ++i)
+    {
+        if (A[i] > *max_value) 
+            *max_value = A[i];
+    }
 }
 
 void avg(double * A, int len, double * avg_value)
 {
-	int i;
-	*avg_value=0;
-	for (i = 0; i < len; ++i)
-		(*avg_value)+=A[i];
-	(*avg_value)=(*avg_value)/len;
+    int i;
+    *avg_value = 0;
+    for (i = 0; i < len; ++i)
+        (*avg_value) += A[i];
+    (*avg_value) = (*avg_value) / len;
 }
 
-void fill(double *A, int n_A, double * B, int nr, int nc, int rth, int cth)
+void fill(double *A, int n_A, double *B, int nr, int nc, int rth, int cth)
 {
-	int m,n,cnt=0;
-	for (m = 0; m<nr; ++m)
-	{
-		for (n = 0; n < nc; ++n)
-		{
-			B[cnt]=A[(rth+m)*n_A+n+cth];
-			++cnt;
-		}
-	}
+    int m, n, cnt = 0;
+    for (m = 0; m < nr; ++m)
+    {
+        for (n = 0; n < nc; ++n)
+        {
+            B[cnt]=A[(rth + m) * n_A + n + cth];
+            ++cnt;
+        }
+    }
 }
 
 void down_sample(double * A, double * B, \
-	int m_A, int n_A, int nr, int nc,\
-	void (*f)(double *, int, double *))
+        int m_A, int n_A, int nr, int nc,\
+        void (*f)(double *, int, double *))
 {
-   int i,j,cnt=0;
-   int len=nr*nc;
-   int m_E_A,n_E_A;
-   double * tmp=(double *)calloc(len,sizeof(double));
-   // double tmp[len];
-   double pool_value;
-   int mod_nr=m_A%nr;
-   int mod_nc=n_A%nc;
-   double * Expand_A;
-   ////padding the boundary if necessary
-   if(mod_nr==0 && mod_nc==0)  
-   {
-		Expand_A=A;
-		m_E_A=m_A;
-		n_E_A=n_A;
-   }
-   else
-   {
-		m_E_A=m_A-mod_nr+nr;
-		n_E_A=n_A-mod_nc+nc;
-   		Expand_A=(double *)calloc(m_E_A*n_E_A,sizeof(double));
-		for(i=0;i<m_A;++i)
-	   	{
-	   		for (j = 0; j<n_A;++j)
-	   		{
-	   			Expand_A[i*n_E_A+j]=A[cnt];
-	   			++cnt;
-	   		}
+    int i, j, cnt = 0;
+    int len = nr * nc;
+    int m_E_A, n_E_A;
+    double *tmp = (double *)calloc(len, sizeof(double));
+    double pool_value;
+    int mod_nr = m_A % nr;
+    int mod_nc = n_A % nc;
+    double *Expand_A;
+    ////padding the boundary if necessary
+    if (mod_nr == 0 && mod_nc == 0)  
+    {
+        Expand_A = A;
+        m_E_A = m_A;
+        n_E_A = n_A;
+    }
+    else
+    {
+        m_E_A = m_A - mod_nr + nr;
+        n_E_A = n_A - mod_nc + nc;
+        Expand_A = (double *)calloc(m_E_A * n_E_A, sizeof(double));
+        for (i = 0; i < m_A; ++i)
+        {
+            for (j = 0; j < n_A; ++j)
+            {
+                Expand_A[i * n_E_A + j] = A[cnt];
+                ++cnt;
+            }
 
-	   	}	
-   }
-   ////
-
-   cnt=0;
-   
-   for(i=0;i<m_E_A;i+=nr)
-   {
-   		for (j = 0; j<n_E_A;j+=nc)
-   		{
-   			 fill(Expand_A,n_E_A,tmp,nr,nc,i,j);
-   			 (*f)(tmp,len,&pool_value);
-	   		 B[cnt]=pool_value;
-   			 ++cnt;
-   		}
+        }	
+    }
+    ////
+    cnt = 0;
+    for (i = 0; i < m_E_A; i += nr)
+    {
+        for (j = 0; j < n_E_A; j += nc)
+        {
+            fill(Expand_A, n_E_A, tmp, nr, nc, i, j);
+            (*f)(tmp, len, &pool_value);
+            B[cnt] = pool_value;
+            ++cnt;
+        }
 
    }
    free(tmp);
-   if(mod_nr!=0 || mod_nc!=0) 
+   if (mod_nr != 0 || mod_nc != 0) 
    free(Expand_A);
 }
 
-void up_sample(double * A, double * B, 
-	int m_A, int n_A, int m_B, int n_B, int nr, int nc)
+void up_sample(double *A, double *B, 
+        int m_A, int n_A, int m_B, int n_B, int nr, int nc)
 {
-	int i,j,m,n;
-	int rm_B=nr*m_A-m_B;
-	int rn_B=nc*n_A-n_B;
-	
-	for(i=0;i<m_A;++i)
-	{
-		for(j=0;j<n_A;++j)
-		{
-			for (m = 0; m < nr; ++m)
-			{
-				for (n = 0; n < nc; ++n)
-					B[(i*nr+m)*n_B+j*nc+n]=A[i*n_A+j];
-			}
-		}
-	}
+    int i, j, m, n;
+    int rm_B = nr * m_A - m_B;
+    int rn_B = nc * n_A - n_B;
+
+    for (i = 0; i < m_A; ++i)
+    {
+        for (j = 0; j < n_A; ++j)
+        {
+            for (m = 0; m < nr; ++m)
+            {
+                for (n = 0; n < nc; ++n)
+                    B[(i * nr + m) * n_B + j * nc + n] = A[i * n_A + j];
+            }
+        }
+    }
 
 }
 
@@ -115,25 +112,25 @@ void up_sample(double * A, double * B,
 
 int main()
 {
-double a[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-int ma=5,na=5;
-int nr=2,nc=2;
-int len=((int)(ma/nr)+1)*((int)(na/nc)+1);
-double b[len];
-down_sample(a, b,ma, na,  nr, nc,max);
-int i;
-for(i=0;i<len;++i)
-	printf("%5.2lf \n",b[i]);
-//  double b[ma*nr*na*nc];
-// up_sample(a,b,ma,na,nr,nc);
-// int i,j;
-//  for(i=0;i<ma*nr;++i)
-// 	{
-//  	 for(j=0;j<na*nc;++j)
-// 		printf("%5.2lf ",b[i*(na*nc)+j]);
-// 	printf("\n");
-// 	}
-return 0;
+    double a[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+    int ma = 5, na = 5;
+    int nr = 2, nc = 2;
+    int len = ((int)(ma / nr) + 1) * ((int)(na / nc) + 1);
+    double b[len];
+    down_sample(a, b, ma, na, nr, nc, max);
+    int i;
+    for (i = 0; i < len; ++i)
+        printf("%5.2lf \n",b[i]);
+    // double b[ma * nr * na * nc];
+    // up_sample(a, b, ma, na, nr, nc);
+    // int i, j;
+    //  for (i = 0; i < ma * nr; ++i)
+    // 	{
+    //  	 for (j = 0; j < na * nc; ++j)
+    // 	        printf("%5.2lf ",b[i * (na * nc) + j]);
+    // 	    printf("\n");
+    // 	}
+    return 0;
 }
 
 
